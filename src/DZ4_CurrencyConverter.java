51ced6864de3ee84b27f61d0bbd1b1e410281ed7
Shadai <Shadaykin@gmail.com>
2023-05-05 01:54:53 +0500

    add HW4

---
+++ src/DZ4_CurrencyConverter.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class DZ4_CurrencyConverter {
+    public static void main(String[] args) {
+        System.out.println("***Конвертер валют***");
+
+        System.out.println("Введите кол-во рублей:");
+        double  rubCount = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите актуальный курс доллара:");
+        double dollarPrice = new Scanner(System.in).nextDouble();
+
+        System.out.println("Введите актуальный курс евро:");
+        double euroPrice = new Scanner(System.in).nextDouble();
+
+        System.out.println(rubCount + " рублей это " + rubCount * (1 / dollarPrice) + " долларов");
+        System.out.println(rubCount + " рублей это " + rubCount * (1 / euroPrice) + " евро");
+    }
+}
